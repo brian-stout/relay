783aaea74ee3817039668f1792a6be8a0579154f
Brian Stout <brianjonstout@gmail.com>
2017-05-12 09:42:14 -0400

    Framework for listener and dispatcher programs.

---
+++ dispatcher.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define SOCK_PATH "bs_socket"
+
+int main(void)
+{
+}
+        
